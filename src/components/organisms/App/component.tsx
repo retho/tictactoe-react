db9533744ee8ee149b069c413c3321068dcb6a47
Evgeny Safonov <safonov.e.s@gmail.com>
2020-11-29 17:53:37 +0300

    .

--- src/components/organisms/App/component.tsx
+++ src/components/organisms/App/component.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import logo from './assets/logo.svg';
+import './styles.scss';
 
 const App: FC = () => {
   return (
