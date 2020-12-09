fa8b2a56ed93afe789f55474776a5d090b86b716
Evgeny Safonov <safonov.e.s@gmail.com>
2020-12-09 16:06:47 +0300

    module.hot

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
-import Root from 'Root';
+import Root from './Root';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,14 @@ const store = configureStore({
   ],
 });
 
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./root', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const {reducer: newRootReducer} = require('./root').default;
+    store.replaceReducer(newRootReducer);
+  });
+}
+
 export type RootState = ReturnType<typeof reducer>;
 export type AppDispatch = typeof store.dispatch;
 
