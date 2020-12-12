8f98c6ca2a5f57c72af6b65fe638a64add46fc25
Evgeny Safonov <safonov.e.s@gmail.com>
2020-12-12 15:23:44 +0300

    NotFoundPage

--- src/components/pages/NotFoundPage/component.tsx
+++ src/components/pages/NotFoundPage/component.tsx
@@ -1,7 +1,17 @@
 import React, {FC} from 'react';
+import {bem} from 'utils/bem';
+import './styles.scss';
 
+const root = bem(module.id, 'NotFoundPage');
 const NotFoundPage: FC = () => {
-  return <div>404 page was not found</div>;
+  return (
+    <main className={root()}>
+      <section>
+        <span>404</span>
+        <p>The requested path could not be found</p>
+      </section>
+    </main>
+  );
 };
 
 export default NotFoundPage;
